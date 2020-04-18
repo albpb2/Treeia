4c0fe320a0afc2aa460982cab088b7f01087e637
albpb2 <albpb2@gmail.com>
2020-04-18 12:12:13 +0200

    Create initial character controller

---
+++ TowerTree/Assets/MainCharacterController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+    [SerializeField] 
+    private float _movementSpeed;
+    
+    private Rigidbody2D _rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var currentPosition = transform.position;
+        var yDelta = 0f;
+        var xDelta = 0f;
+        
+        if (verticalInput > 0)
+        {
+            yDelta = _movementSpeed;
+        }
+        else if (verticalInput < 0)
+        {
+            yDelta = -_movementSpeed;
+        }
+
+        if (horizontalInput > 0)
+        {
+            xDelta = _movementSpeed;
+        }
+        else if (horizontalInput < 0)
+        {
+            xDelta = -_movementSpeed;
+        }
+        
+        _rigidbody.MovePosition(new Vector2(currentPosition.x + xDelta, currentPosition.y + yDelta));
+    }
+}
