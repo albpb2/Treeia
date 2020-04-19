8cb57930bea87a5e86b36bb618e57fefe85ca147
albpb2 <albpb2@gmail.com>
2020-04-19 19:07:27 +0200

    Create main menu scene

---
+++ TowerTree/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        var dontDestroyOnLoadScene = UnityEngine.SceneManagement.SceneManager.GetSceneByName("DontDestroyOnLoad");
+        if (dontDestroyOnLoadScene != null && dontDestroyOnLoadScene.isLoaded)
+        {
+            var gameObjects = dontDestroyOnLoadScene.GetRootGameObjects();
+            if (gameObjects != null)
+            {
+                for (var i = 0; i < gameObjects.Length; i++)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PreLoadScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
